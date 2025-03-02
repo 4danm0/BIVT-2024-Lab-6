4cb548f67ed06f95e5d52cdd7ab9db33e3accba4
4danm0 <89018902+4danm0@users.noreply.github.com>
2025-03-03 02:34:10 +0300

    Add files via upload

---
+++ Purple_3.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    public class Purple_3
+    {
+        public struct Participant
+        {
+            private string _name;
+            private string _surname;
+            private double[] _marks;
+            private int[] _places;
+
+            private int _marksCount;
+            public string Name => _name;
+            public string Surname => _surname;
+            public double[] Marks
+            {
+                get
+                {
+                    if (_marks == null) return default(double[]);
+
+                    var newArray = new double[_marks.Length];
+                    Array.Copy(_marks, newArray, _marks.Length);
+                    return newArray;
+                }
+            }
+            public int[] Places
+            {
+                get
+                {
+                    if (_places == null) return default(int[]);
+
+                    var newArray = new int[_places.Length];
+                    Array.Copy(_places, newArray, _places.Length);
+                    return newArray;
+                }
+            }
+            public int Score => _places == null ? 0 : _places.Sum();
+            public Participant(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _marks = new double[7];
+                _places = new int[7];
+                _marksCount = 0;
+            }
+
+            public void Evaluate(double result)
+            {
+                if (_marksCount == 7 || _marks == null) return;
+                _marks[_marksCount++]  = result;
+            }
+
+            public static void SetPlaces(Participant[] participants)
+            {
+                if (participants == null) return;
+                for (int judgeIndex = 0; judgeIndex < 7; judgeIndex++)
+                {
+                    SortJudge(participants, judgeIndex);
+                    for (int i = 0; i < participants.Length; i++)
+                    {
+                        participants[i]._places[judgeIndex] = i + 1;
+                    }
+                }
+            }
+            private static void SortJudge(Participant[] array, int judgeIndex)
+            {
+                if (array == null) return;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = 0; j < array.Length - i - 1; j++)
+                    {
+                        if (array[j].Marks[judgeIndex] > array[j + 1].Marks[judgeIndex])
+                        {
+                            var t = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = t;
+                        }
+                    }
+                }
+            }
+
+            public static void Sort(Participant[] array)
+            {
+                if (array == null) return;
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = 0; j < array.Length - i - 1; j++)
+                    {
+                        if (Compare(array[j], array[j+1]))
+                        {
+                            var t = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = t;
+                        }
+
+                    }
+                }
+            }
+            private static bool Compare(Participant p1, Participant p2)
+            {
+                if (p1.Score != p2.Score) return p1.Score > p2.Score;
+                if (p1.Places.Min() != p2.Places.Min()) return p1.Places.Min() < p2.Places.Min(); 
+                return p1.Marks.Sum() > p2.Marks.Sum(); 
+            }
+            public void Print()
+            {
+                Console.WriteLine(_name + " " + _surname);
+                Console.Write("Marks: ");
+                foreach (double var in _marks)
+                {
+                    Console.Write(var + "  ");
+                }
+                Console.Write("Places: ");
+                foreach (double var in _places)
+                {
+                    Console.Write(var + "  ");
+                }
+                Console.WriteLine($"Score: {Score}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Purple_4.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    public class Purple_4
+    {
+        public struct Sportsman
+        {
+            private string _name;
+            private string _surname;
+            private double _time;
+
+            public string Name => _name;
+            public string Surname => _surname;
+            public double Time => _time;
+
+            public Sportsman(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _time = 0.0;
+            }
+
+            public void Run(double time)
+            {
+                if (_time != 0.0) return;
+                _time = time;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine(_name + " " + _surname);
+                Console.WriteLine($"Time: {_time}");
+                Console.WriteLine();
+            }
+        }
+        public struct Group
+        {
+            private string _name;
+            private Sportsman[] _teammates;
+
+            public string Name => _name;
+            public Sportsman[] Sportsmen
+            {
+                get
+                {
+                    if (_teammates == null) return default(Sportsman[]);
+
+                    var newArray = new Sportsman[_teammates.Length];
+                    Array.Copy(_teammates, newArray, _teammates.Length);
+                    return newArray;
+                }
+            }
+
+            public Group(string name)
+            {
+                _name = name;
+                _teammates = new Sportsman[0]; 
+            }
+            public Group(Group group)
+            {
+                _name = group.Name;
+                if (group.Sportsmen == null)
+                {
+                    _teammates = new Sportsman[0];
+                    return;
+                }
+                _teammates = new Sportsman[group.Sportsmen.Length];
+                Array.Copy(group.Sportsmen, _teammates, group.Sportsmen.Length);
+            }
+
+            public void Add(Sportsman newSportsman)
+            {
+                if (_teammates == null) return;
+                Array.Resize(ref _teammates, _teammates.Length + 1); 
+                _teammates[_teammates.Length - 1] = newSportsman;
+            }
+
+            public void Add(Sportsman[] newSportsmen)
+            {
+                if (newSportsmen == null || _teammates == null) return;
+                int oldLength = _teammates.Length;
+                Array.Resize(ref _teammates, _teammates.Length + newSportsmen.Length);
+                Array.Copy(newSportsmen, 0, _teammates, oldLength, newSportsmen.Length); 
+            }
+
+            public void Add(Group group) 
+            {
+                if (group.Sportsmen == null || _teammates == null) return;
+                int oldLength = _teammates.Length;
+                Array.Resize(ref _teammates, _teammates.Length + group.Sportsmen.Length); 
+                Array.Copy(group.Sportsmen, 0, _teammates, oldLength, group.Sportsmen.Length);
+            }
+
+            public void Sort()
+            {
+                if (_teammates == null) return;
+
+                for (int i = 0; i < _teammates.Length; i++)
+                {
+                    for (int j = 0; j < _teammates.Length - i - 1; j++)
+                    {
+                        if (_teammates[j].Time > _teammates[j+1].Time)
+                        {
+                            var t = _teammates[j];
+                            _teammates[j] = _teammates[j + 1];
+                            _teammates[j + 1] = t;
+                        }
+                    }
+                }
+            }
+
+            public static Group Merge(Group group1, Group group2)
+            {
+                if (group1.Sportsmen == null || group2.Sportsmen == null) return default(Group); 
+                Group finalists = new Group("Финалисты");
+
+                group1.Sort();
+                group2.Sort();
+
+                int i = 0, j = 0;
+                while (i < group1.Sportsmen.Length && j < group2.Sportsmen.Length)
+                {
+                    if (group1.Sportsmen[i].Time <= group2.Sportsmen[j].Time)
+                        finalists.Add(group1.Sportsmen[i++]);
+
+                    else
+                        finalists.Add(group2.Sportsmen[j++]);
+                }
+
+                while (i < group1.Sportsmen.Length)
+                    finalists.Add(group1.Sportsmen[i++]);
+
+                while (j < group2.Sportsmen.Length)
+                    finalists.Add(group2.Sportsmen[j++]);
+
+                return finalists;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"Group name: {_name}");
+                foreach (Sportsman sportsman in _teammates)
+                {
+                    sportsman.Print();
+                }
+            }
+        }
+    }
+}
